7bcee83abf0fbbfd15ef44d5d2d74018ee18dc2d
Vivian Roest <vivian@0x76.dev>
2022-11-15 16:41:53 +0100

    add unprotect command

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use clap::{Parser, Subcommand};
-use dotenv;
 use gitlab::Gitlab;
 
 mod projects;
 
 #[derive(Parser, Debug)]
 struct Args {
+    /// Gitlab host
     #[arg(long, default_value = "gitlab.ewi.tudelft.nl")]
     host: String,
 
+    /// Gitlab API token
     #[arg(long, env = "GITLAB_TOKEN", hide_env_values = true)]
     token: String,
 
@@ -18,9 +19,22 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum SubCommand {
+    /// Lists all projects in a given group with ssh clone url
     Projects {
+        /// Gitlab group ID
         #[arg(required = true)]
-        id: u64,
+        group_id: u64,
+    },
+
+    /// Unprotects a given branch on all projects in a given group
+    Unprotect {
+        /// Gitlab group ID
+        #[arg(required = true)]
+        group_id: u64,
+
+        /// Branch to unprotect
+        #[arg(required = true)]
+        branch: String,
     },
 }
 
@@ -31,6 +45,9 @@ fn main() {
     let client = Gitlab::new(args.host, args.token).unwrap();
 
     match args.cmd {
-        SubCommand::Projects { id } => projects::list_projects(&client, id),
+        SubCommand::Projects { group_id} => projects::list(&client, group_id),
+        SubCommand::Unprotect { group_id, branch } => {
+            projects::unprotect(&client, group_id, &branch);
+        }
     }
 }
--- src/projects.rs
+++ src/projects.rs
@@ -1,21 +1,53 @@
 use gitlab::{
-    api::{groups::projects::GroupProjects, Query},
-    Gitlab,
+    api::{
+        groups::projects::GroupProjects, ignore, paged,
+        projects::protected_branches::UnprotectBranch, Query,
+    },
+    Gitlab, ProjectId,
 };
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct Project {
+    id: ProjectId,
     name: String,
     ssh_url_to_repo: String,
 }
 
-pub fn list_projects(client: &Gitlab, id: u64) {
-    let endpoint = GroupProjects::builder().group(id).build().unwrap();
+fn get_projects_by_group(client: &Gitlab, id: u64) -> Vec<Project> {
+    let endpoint = GroupProjects::builder()
+        .group(id)
+        .archived(false)
+        .build()
+        .unwrap();
 
-    let projects: Vec<Project> = endpoint.query(client).unwrap();
+    paged(endpoint, gitlab::api::Pagination::All)
+        .query(client)
+        .unwrap()
+}
+
+pub fn list(client: &Gitlab, id: u64) {
+    let projects = get_projects_by_group(client, id);
+
     for project in projects {
         let name = project.name.replace(' ', "-").to_lowercase();
         println!("{name} {}", project.ssh_url_to_repo);
     }
+}
+
+pub fn unprotect(client: &Gitlab, group: u64, branch: &str) {
+    let projects = get_projects_by_group(client, group);
+    let n = projects.len();
+
+    for project in projects {
+        let endpoint = UnprotectBranch::builder()
+            .project(project.id.value())
+            .name(branch)
+            .build()
+            .unwrap();
+
+        // TODO: Ignore if branch already unprotected / doesn't exist
+        ignore(endpoint).query(client).unwrap();
+    }
+    println!("Unprotected {branch} on {n} projects successfully");
 }
