27326e567240ccf5d1359ee92854a0f0de40ab28
Vivian Roest <vivian@0x76.dev>
2022-11-17 13:26:03 +0100

    add unassign parameter

--- scripts/create_fix_mr.sh
+++ scripts/create_fix_mr.sh
@@ -5,6 +5,7 @@ cd "$repo"
 
 # CONFIG
 REMOTES=$(git remote | grep --invert-match origin)
+OWN_USER_ID=1027
 SOURCE_BRANCH=1-concurrency-update
 TARGET_BRANCH=1-concurrency
 TITLE="Fix example 3"
@@ -12,5 +13,5 @@ DESCRIPTION="We made a mistake in Example 3 which can cause issues when trying t
 # END CONFIG
 
 while read -r target_remote; do
-	git push -o merge_request.create -o merge_request.target="$TARGET_BRANCH" -o merge_request.title="$TITLE" -o merge_request.description="$DESCRIPTION" -o merge_request.remove_source_branch="true" "$target_remote" "$SOURCE_BRANCH"
+	git push -o merge_request.create -o merge_request.unassign="$OWN_USER_ID" -o merge_request.target="$TARGET_BRANCH" -o merge_request.title="$TITLE" -o merge_request.description="$DESCRIPTION" -o merge_request.remove_source_branch="true" "$target_remote" "$SOURCE_BRANCH"
 done <<<"$REMOTES"
